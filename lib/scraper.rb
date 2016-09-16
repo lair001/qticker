2a64ca20e5973c5bc51bf61ba2213d6a8d570e10
Samuel Lair <lair001@gmail.com>
2016-09-15 21:08:48 -0400

    fixed scraping code

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,24 +17,24 @@ class Scraper
 		true
 	end
 
-	# def create_stock
-	# 	name = pry(main)> noko.css("div.g-first a").text.match('(?<=All news for ).*(?= »)')[0]
-	# 	exchange = noko.css("span.dis-large").text.split("\n")[0]
-	# 	price = self.gfs_noko_html("span.pr").text.strip
-	# 	change = noko.css("div.nwp span.bld").text.split("\n")[0]
-	# 	change_pct = noko.css("div.nwp span.bld").text.split("\n")[1].gsub(/[)(]/, '')
-	# 	range = noko.css("td[data-snapfield='range']+td").text.strip
-	# 	range_yr = noko.css("td[data-snapfield='range_52week']+td").text.strip
-	# 	open = noko.css("td[data-snapfield='open']+td").text.strip
-	# 	volume = noko.css("td[data-snapfield='vol_and_avg']+td").text.strip.split("/")[0]
-	# 	volume_avg = noko.css("td[data-snapfield='vol_and_avg']+td").text.strip.split("/")[1]
-	# 	mkt_cap = noko.css("td[data-snapfield='market_cap']+td").text.strip
-	# 	pe_ttm = noko.css("td[data-snapfield='pe_ratio']+td").text.strip
-	# 	div_yld = noko.css("td[data-snapfield='latest_dividend-dividend_yield']+td").text.strip.split("/")[1] + "%"
-	# 	sector = noko.css("a#sector").text
-	# 	industry = noko.css("a#sector+a").text
-	# 	desc = pry(main)> noko.css("div.companySummary").text.gsub("More from Reuters »", "").strip
+	def create_stock
+		name = pry(main)> self.gfs_noko_html.css("div.g-first a").text.match('(?<=All news for ).*(?= »)')[0]
+		exchange = self.gfs_noko_html.css("span.dis-large").text.split("\n")[0]
+		price = self.gfs_noko_html.css("span.pr").text.strip
+		change = self.gfs_noko_html.css("div.nwp span.bld").text.split("\n")[0]
+		change_pct = self.gfs_noko_html.css("div.nwp span.bld").text.split("\n")[1].gsub(/[)(]/, '')
+		range = self.gfs_noko_html.css("td[data-snapfield='range']+td").text.strip
+		range_yr = self.gfs_noko_html.css("td[data-snapfield='range_52week']+td").text.strip
+		open = self.gfs_noko_html.css("td[data-snapfield='open']+td").text.strip
+		volume = self.gfs_noko_html.css("td[data-snapfield='vol_and_avg']+td").text.strip.split("/")[0]
+		volume_avg = self.gfs_noko_html.css("td[data-snapfield='vol_and_avg']+td").text.strip.split("/")[1]
+		mkt_cap = self.gfs_noko_html.css("td[data-snapfield='market_cap']+td").text.strip
+		pe_ttm = self.gfs_noko_html.css("td[data-snapfield='pe_ratio']+td").text.strip
+		div_yld = self.gfs_noko_html.css("td[data-snapfield='latest_dividend-dividend_yield']+td").text.strip.split("/")[1] + "%"
+		sector = self.gfs_noko_html.css("a#sector").text
+		industry = self.gfs_noko_html.css("a#sector+a").text
+		desc = self.gfs_noko_html.css("div.companySummary").text.gsub("More from Reuters »", "").strip
 
-	# end
+	end
 
 end
